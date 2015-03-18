c4574a0aaf6403d9c2102892c558a4f46b055b5a
Dima Osptapenko <dostapn@gmail.com>
2015-03-18 11:32:16 +0300

    добавлены еще стили

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,5 @@
 jQuery(function() {
-  $("a[rel~=popover], .has-popover").popover();
-  $("a[rel~=tooltip], .has-tooltip").tooltip();
+    $("a[rel~=popover], .has-popover").popover();
+    $('[data-toggle="tooltip"]').tooltip();
 });
+
