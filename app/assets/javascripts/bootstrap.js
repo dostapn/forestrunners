58083b2bcc38ec313dcbeecbbd0a875681137912
Dima Osptapenko <dostapn@gmail.com>
2015-03-18 21:39:10 +0300

    navbar popup complete

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,6 +1,46 @@
 jQuery(function() {
     $("a[rel~=popover], .has-popover").popover();
     $('[data-toggle="tooltip"]').tooltip();
+
+    // Navigation Scripts to Show Header on Scroll-Up
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var prevTop = 0;
+        var headerHeight = $(".navbar-custom").height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+                //check if scrolling up
+                if (currentTop < prevTop) {
+                    //if scrolling up...
+                    if ((currentTop > 0) && $(".navbar-custom").hasClass("is-fixed")) {
+                        $(".navbar-custom").addClass("is-visible");
+                        console.log("addClass is-visible");
+                    } else {
+                        $(".navbar-custom").removeClass("is-fixed is-visible");
+                        console.log("removeClass is-fixed is-visible");
+                    }
+                } else {
+                    //if scrolling down...
+                    $(".navbar-custom").removeClass("is-visible");
+                    console.log("removeClass is-visible");
+                    if (currentTop > headerHeight && !$(".navbar-custom").hasClass("is-fixed")) {
+                        $(".navbar-custom").addClass("is-fixed");
+                        console.log("addClass is-fixed");
+                    }
+                }
+                console.log("prevTop = " + prevTop);
+                console.log("previousTop = " + this.previousTop);
+                console.log("cuurentTop = " + currentTop);
+                prevTop = this.previousTop;
+                this.previousTop = currentTop;
+            }
+        );
+    }
 });
 
 
