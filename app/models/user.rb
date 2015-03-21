e31eac80401e6ebe69900fe07aadfa4ac9dc37ae
Dima Osptapenko <dostapn@gmail.com>
2015-03-21 21:16:41 +0300

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable
+
+  validates :login,             :presence => true, :uniqueness => true
 end
